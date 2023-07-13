c53f0c51ce8968699176eeec9aaa25c65234f8e4
Kemoel Amorim Miranda <kemoel.amorim@workdb.com.br>
2023-07-13 14:00:19 -0300

    condicionais

--- conditional.js
+++ conditional.js
@@ -4,8 +4,8 @@ console.log(name)
 var age = 19 // int
 console.log("I am" , age)
 
-let adult = false
-if (adult) {
+let adult = 18
+if (adult >= age) {
     console.log("I am an adult")
 } else{
     console.log("I am not an adult")
