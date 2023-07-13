ac8e00078b56310720dbf327e8ab3936a38d036f
Kemoel Amorim Miranda <kemoel.amorim@workdb.com.br>
2023-07-13 14:02:53 -0300

    estruturas de repetiçoes up

--- conditional.js
+++ conditional.js
@@ -1,18 +1,13 @@
 const name = "Sidney" //string
 console.log(name)
 
-var age = 19 // int
+var age = 17 // int
 console.log("I am" , age)
 
-<<<<<<< HEAD
-let adult = 18
-if (adult >= age) {
-=======
 var accomp = true
 
 let adultAge = 18
 if (age >= adultAge) {
->>>>>>> 13f10222b82736c034f1c30a425ecab8d856fe28
     console.log("I am an adult")
 } else{
     console.log("I am not an adult")
