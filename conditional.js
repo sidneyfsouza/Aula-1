a5ad0fe62ffb63f95982f7a83d3dbd662fa3387b
sidneyfsouza <sidney.fox@hotmail.com>
2023-07-13 09:52:16 -0300

    July 13th

---
+++ conditional.js
@@ -0,0 +1,12 @@
+const name = "Sidney" //string
+console.log(name)
+
+var age = 19 // int
+console.log("I am" , age)
+
+let adult = false
+if (adult) {
+    console.log("I am an adult")
+} else{
+    console.log("I am not an adult")
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+const name = "Sidney" //string
+console.log("My name is " + name)
+
+var age = 33 // int
+console.log("I am" , age)
+
+var job = "teacher" //string
+console.log("I am an English" , job)
+
+let employed = false //boolean
+if (!employed) {
+    console.log("I am not working")
+}else {
+    console.log("I am working")
+}
+
+var income = 5720.50 //float or double
+console.log("my income is $" , income)
+
+//My name is Sidney. I am 33 and I am an English teacher. I am not employed right now and my income is $ 5720.50.
