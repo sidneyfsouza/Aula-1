04b46838af689c4d64bb8bffdec1c1adb0b6c402
sidneyfsouza <sidney.fox@hotmail.com>
2023-07-24 11:24:24 -0300

    For, While, Do (while)

--- repetition.js
+++ repetition.js
@@ -1,16 +1,34 @@
 var numbers = [1,2,3,4,5]
 console.log(numbers)
 
-var fruits = ["apple", "grapes", "strawberry", "raspberry", "blackberry"]
-console.log(fruits[1])
-
+const fruits = ["apple", "grapes", "strawberry", "raspberry", "blackberry"]
+console.log(fruits)
+var fruit
 for (let i = 0; i < fruits.length; i++) {
     fruit = fruits[i];
+    console.log(fruit)
     if (fruits[1] == fruit){
         console.log(fruit)    
-    }
-    var nome = "banana"    
+    }  
 }
-if (true){
+const nomes = ["John", "Kelly", "Lina", "Mike", "Janice", "Kyle"]
+console.log(nomes)
+var nome;
+/*for (let i = 0; i< nomes.length; i++) {
+    nome = nomes[i]
     console.log(nome)
+}*/
+
+let i = 0
+while(i< nomes.length) {
+    
+    console.log(nomes[i]) 
+    i++
 }
+const cardapio = ["sarapatel", "buchada de bode", "carne de sol", "pudim"]
+let indice = 0
+console.log("restaurante da Mary") 
+do{
+    console.log(cardapio[indice]) 
+    indice++
+} while(indice < cardapio.length)
--- variaveis.js
+++ variaveis.js
@@ -1,23 +1,27 @@
 // Exemplo usando 'let'
 let x = 10;
+let age = 33
 if (x > 5) {
   let y = 20;
-  console.log(x, y);  // Output: 10, 20
+  let nome = "Sidney"
+  console.log(x, age, y);  // Output: 10, 20
 }
-console.log(x, y);    // Erro: y is not defined (y não está definido)
+console.log(x);    // Erro: y is not defined (y não está definido)
 
 // Exemplo usando 'var'
 var a = 10;
 if (a > 5) {
   var b = 20;
+  var nome = "Sid"
   console.log(a, b);  // Output: 10, 20
 }
-console.log(a, b);    // Output: 10, 20 (b está acessível fora do bloco if)
+console.log(a, b, nome);    // Output: 10, 20 (b está acessível fora do bloco if)
 
 // Exemplo usando 'const'
 const p = 10;
 if (p > 5) {
   const q = 20;
+  const p = 5;
   console.log(p, q);  // Output: 10, 20
 }
 console.log(p, q);    // Erro: q is not defined (q não está definido)
